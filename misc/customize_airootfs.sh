79dfe078be590e83efc6c4265d39b2aa304e17f9
sentothecopycat <sotentothecopycat@gmail.com>
2025-01-16 19:44:29 +0000

    fixing fixing

--- misc/customize_airootfs.sh
+++ misc/customize_airootfs.sh
@@ -1,7 +1,8 @@
 flatpak install citra net.davidotek.pupgui2
-systemctl enable NetworkManager org.cups.cupsd lvm2-monitor avahi-daemon spice-vdagentd systemd-resolved upower || true
+systemctl enable NetworkManager cups lvm2-monitor avahi-daemon spice-vdagentd systemd-resolved upower || true
 systemctl --global enable pipewire || true
 systemctl disable multi-user.target
 systemctl set-default graphical.target
-useradd -c \'Live User\' -G users,wheel -m liveuser
+#-c \'Live User\'
+useradd -G users,wheel -m liveuser
 passwd -d liveuser
